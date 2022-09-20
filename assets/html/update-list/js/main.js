c72935a60cff3eff0d6ac16f5b5a6e14dc3039c2
Chuazz <anhsonbeo97@gmail.com>
2022-09-20 15:12:44 +0700

    Thay doi co che render danh sach

--- assets/html/update-list/js/main.js
+++ assets/html/update-list/js/main.js
@@ -1,9 +1,32 @@
 import { renderSong } from "../../../js/module/function.js";
-import { removeContainer, songList } from "../../../js/module/variable.js";
+import { $$, removeContainer, songAPI, totalSongRemove } from "../../../js/module/variable.js";
 
 const app = {
-    start: function(){
-        renderSong(songList, removeContainer);
+    // Lấy danh sách bài hát
+    loadSongList: function () {
+        fetch(songAPI)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                renderSong(data, removeContainer);
+            })
+    },
+
+    handleEvents: function () {
+        removeContainer.addEventListener("click", (e) => {
+            var songItem = e.target.closest(".song__item-body");
+
+            if(songItem){
+                songItem.classList.toggle("active");
+                totalSongRemove.innerHTML = $$(".song__item-body.active").length;
+            }
+        })
+    },
+
+    start: function () {
+        this.loadSongList();
+        this.handleEvents();
     }
 }
 
